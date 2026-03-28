cbff79d16b159f5ebbc9b1d952994bb19c5a2443
Sven Bardos <sven.bardos@googlemail.com>
2026-03-28 19:03:57 +0100

    Support holes in maps

--- assets/Maps/Base/pathfind.gd
+++ assets/Maps/Base/pathfind.gd
@@ -17,7 +17,9 @@ func _ready() -> void:
 	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
 	astar_grid.update()
 	
-	load_wall()
+	load_tilemap_props(map_floor, true)
+	load_tilemap_props(map_walls, false)
+	load_tilemap_props(map_interior, false )
 
 
 func _process(_delta: float) -> void:
@@ -26,15 +28,23 @@ func _process(_delta: float) -> void:
 func remove_character(cell_pos: Vector2i):
 	astar_grid.set_point_solid(cell_pos, false)
 
-
 func add_character(cell_pos: Vector2i):
 	astar_grid.set_point_solid(cell_pos, true)
 	
-func load_wall():
-	var cell_positions = map_walls.get_used_cells()
+func load_tilemap_props(tilemapLayer : TileMapLayer, check_empty_cells : bool):
+	var grid_size = tilemapLayer.get_used_rect().size
+	var grid_pos = tilemapLayer.get_used_rect().position
 	
-	for cell_pos in cell_positions:
-		var cell = map_walls.get_cell_tile_data(cell_pos)
-		
-		if bool(cell.get_custom_data("IsBlocked")):
-			astar_grid.set_point_solid(cell_pos, true)
+	for i in grid_size.x + grid_pos.x:
+		for j in grid_size.y + grid_pos.y:
+			var astar_grid_pos = Vector2i(i,j)
+			var cell = tilemapLayer.get_cell_tile_data(astar_grid_pos)
+						
+			if check_empty_cells && cell == null:
+				astar_grid.set_point_solid(astar_grid_pos, true)
+			
+			if cell == null:
+				continue
+			
+			if  bool(cell.get_custom_data("IsBlocked")):
+				astar_grid.set_point_solid(astar_grid_pos, true)
