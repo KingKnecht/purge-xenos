b159f3c3216a23ff6454e6a9e8a793e34c8e0915
nanoschi <78222596+Nanoschi@users.noreply.github.com>
2026-03-30 13:30:48 +0200

    render to sub viewport

--- assets/UI/HUD/hud.gd
+++ assets/UI/HUD/hud.gd
@@ -4,16 +4,6 @@ extends CanvasLayer
 
 signal player_turn_end(player : BaseCharacter)
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_btn_end_turn_pressed() -> void:
 	if Player == null:
 		push_error("Player is not set")
