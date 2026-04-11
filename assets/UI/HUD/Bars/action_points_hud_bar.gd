46177f80dfef4e307cc6ed7f07d5aa702c8bb248
Sven Bardos <sven.bardos@googlemail.com>
2026-04-11 12:02:43 +0200

    HUD ActionsPointBar

---
+++ assets/UI/HUD/Bars/action_points_hud_bar.gd
@@ -0,0 +1,17 @@
+extends ColorRect
+class_name ActionPointsHudBar
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func set_points(value : int):
+	material.set_shader_parameter("discrete_fill_amount", value)
+
+func set_max_points(value : int):
+	material.set_shader_parameter("segment_count", value)
--- assets/UI/HUD/hud.gd
+++ assets/UI/HUD/hud.gd
@@ -3,8 +3,9 @@ extends CanvasLayer
 
 @onready var label_action_count = $VertAlign/MenuPanel/LabelActionCount
 @onready var container_action_buttons = $"VertAlign/MenuPanel/PanelContainer/ActionButtons"
+@onready var action_points_bar = $VertAlign/MenuPanel/ActionPointsHudBar as ActionPointsHudBar
 
-var active_player : BaseCharacter
+var active_player : Player
 var action_button_theme : Resource
 
 func _ready() -> void:
@@ -17,13 +18,17 @@ func _process(_delta: float) -> void:
 	if active_player == null:
 		return
 	label_action_count.text = "Remaining Actions: %s" % active_player.action_count
-
+	action_points_bar.set_points(active_player.action_count)
+	
 func on_character_begin_turn(player : BaseCharacter) -> void:
 	# The buttons will currently be recreated every turn.
 	# Pros: Buttons are untoggled again
 	# Cons: Performance?
 	
-	active_player = player
+	active_player = player as Player
+	
+	action_points_bar.set_max_points(active_player.max_action_count)
+	
 	for button in container_action_buttons.get_children():
 		button.queue_free()
 	
