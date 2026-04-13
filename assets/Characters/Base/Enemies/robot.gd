edfd9142cf275ea2b5943cccdbf2933a5a45fb90
Sven Bardos <sven.bardos@googlemail.com>
2026-04-13 20:41:18 +0200

    enemy uses weapon_range for LoS

--- assets/Characters/Base/Enemies/robot.gd
+++ assets/Characters/Base/Enemies/robot.gd
@@ -1,9 +1,9 @@
 extends EnemyBase
 class_name Robot
 
-const ROBOT_SCENE : PackedScene = preload("res://assets/Characters/Base/Enemies/Robot.tscn")
+const ROBOT_SCENE: PackedScene = preload("res://assets/Characters/Base/Enemies/Robot.tscn")
 
-var max_action_count : int
+var max_action_count: int
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -20,28 +20,28 @@ func _ready() -> void:
 	SignalBus.battle_started.connect(_on_battle_started)
 	
 	
-func _on_before_action_executed(character : BaseCharacter, action : CombatAction):
+func _on_before_action_executed(character: BaseCharacter, action: CombatAction):
 	if not has_battle_started:
 		return
 	if character != self:
 		selected_action = combat_actions[CombatAction.ActionType.WAIT]
-		SignalBus.enemy_selected_action.emit(self, selected_action)
+		SignalBus.enemy_selected_action.emit(self , selected_action)
 	
-func _on_after_action_executed(character : BaseCharacter, action : CombatAction):
+func _on_after_action_executed(character: BaseCharacter, action: CombatAction):
 	if not has_battle_started:
 		return
 	if character != self:
 		selected_action = ai_select_action()
-		SignalBus.enemy_selected_action.emit(self, selected_action)
+		SignalBus.enemy_selected_action.emit(self , selected_action)
 
 ## Creates an instance of a pre-configured Robot
-static func create(base_map : BaseMap, max_action_count : int, current_cell : Vector2i) -> BaseCharacter:
-	var robot =  ROBOT_SCENE.instantiate() as Robot
+static func create(base_map: BaseMap, max_action_count: int, current_cell: Vector2i) -> BaseCharacter:
+	var robot = ROBOT_SCENE.instantiate() as Robot
 	robot.base_map = base_map
 	robot.max_action_count = max_action_count
 	robot.current_cell = current_cell
 	
-	var actions : Dictionary[CombatAction.ActionType, CombatAction] = {}
+	var actions: Dictionary[CombatAction.ActionType, CombatAction] = {}
 	var move = CombatAction.create_move_action(5)
 	var pewpew = CombatAction.create_pewpew_action()
 	var wait = CombatAction.create_wait_action()
@@ -53,37 +53,36 @@ static func create(base_map : BaseMap, max_action_count : int, current_cell : Ve
 	return robot
 
 func ai_select_action() -> CombatAction:
-	#var idx : int = randi_range(0, combat_actions.size() - 1)
+	var action: CombatAction
+	var player: BaseCharacter = $"../Player"
 	
-	# Magic AI which provide fully fleshed out actions
-	var action : CombatAction
-	var player : BaseCharacter = $"../Player"
-	var lines_of_sights = base_map.get_los_to_enemies(current_cell, player.get_groups()[0]) #Currently there is only 1 group.
+	var pewpews = combat_actions.values().filter(func(a: CombatAction): return a.action_type == CombatAction.ActionType.PEW_PEW)
+	var pew_range: int = pewpews[0].weapon_range if pewpews.size() > 0 else 0
+	
+	var lines_of_sights = base_map.get_los_to_enemies(current_cell, player.get_groups()[0], pew_range)
 	if lines_of_sights.size() > 0:
 		var nearest_los = lines_of_sights.reduce(func(a, b): return a if Vector2(a[0], a[1]).length_squared() < Vector2(b[0], b[1]).length_squared() else b)
-		var pewpews = combat_actions.values().filter(func(a : CombatAction): return a.action_type == CombatAction.ActionType.PEW_PEW)
 		if pewpews.size() > 0:
 			action = pewpews[0]
-			action.targeted_cells = [MapHelpers.pixel_to_cell(nearest_los[1])] # [1] is the target of the LoS
-				
+			action.targeted_cells = [MapHelpers.pixel_to_cell(nearest_los[1])]
+			
 	if action == null:
-		action = combat_actions[combat_actions.keys()[0]] # Move action
+		action = combat_actions[combat_actions.keys()[0]]
 		action.targeted_cells = [player.current_cell] as Array[Vector2i]
 	
 	var path = base_map.get_astar_path(current_cell, player.current_cell, true)
 	if path.size() > action.movement + 1:
-		path = path.slice(0, action.movement + 1) 
+		path = path.slice(0, action.movement + 1)
 		
 	action.path = path
-
 	return action
 
 func _on_pre_begin_turn():
 	selected_action = ai_select_action()
-	SignalBus.enemy_selected_action.emit(self, selected_action)
+	SignalBus.enemy_selected_action.emit(self , selected_action)
 	
 func start_turn():
-	Log.debug("Turn started for '%s'" % [self])
+	Log.debug("Turn started for '%s'" % [ self ])
 	action_point_bar.set_max_points(max_action_count)
 	action_count = max_action_count
 	
@@ -91,27 +90,26 @@ func start_turn():
 		await get_tree().create_timer(1.0).timeout
 		execute_action(Vector2i(0, 0))
 
-func execute_deal_damage(targeted_cells : Array[Vector2i], damage : int):
+func execute_deal_damage(targeted_cells: Array[Vector2i], damage: int):
 	if damage == 0:
 		return
 	action_count -= selected_action.cost
-	Log.debug("Dealt damage to %s, amount: %d" % [str(targeted_cells), damage ])	
+	Log.debug("Dealt damage to %s, amount: %d" % [str(targeted_cells), damage])
 	
 func execute_action(target: Vector2i):
-	
 	selected_action = ai_select_action()
 	
-	SignalBus.enemy_selected_action.emit(self, selected_action)
+	SignalBus.enemy_selected_action.emit(self , selected_action)
 	
 	var executor = ActionExecutor.new([selected_action])
-	executor.excecute(self)
+	executor.excecute(self )
 	
 	if EnumHelpers.has_flag(CombatAction.ValidTargetFlags.SELF, selected_action.valid_target_flags):
 		Log.debug("Enemy has done: %s" % selected_action.display_name)
-		SignalBus.after_action_executed.emit(self,selected_action)
+		SignalBus.after_action_executed.emit(self , selected_action)
 	elif EnumHelpers.has_flag(CombatAction.ValidTargetFlags.OPPONENTS, selected_action.valid_target_flags):
 		Log.debug("Enemy has done: %s" % selected_action.display_name)
-		SignalBus.after_action_executed.emit(self,selected_action)
+		SignalBus.after_action_executed.emit(self , selected_action)
 	elif EnumHelpers.has_flag(CombatAction.ValidTargetFlags.CELL, selected_action.valid_target_flags):
 		Log.debug("Enemy has done: %s" % selected_action.display_name)
-		SignalBus.after_action_executed.emit(self,selected_action)
+		SignalBus.after_action_executed.emit(self , selected_action)
--- assets/Maps/Base/base_map.gd
+++ assets/Maps/Base/base_map.gd
@@ -1,10 +1,10 @@
 extends Node2D
 class_name BaseMap
 
-const CELL_DEBUG_LABEL_SCENE : PackedScene = preload("uid://s05eepwpbj7e")
+const CELL_DEBUG_LABEL_SCENE: PackedScene = preload("uid://s05eepwpbj7e")
 
 @export var pathfind: Pathfind
-@export var show_debug_cells : bool = false
+@export var show_debug_cells: bool = false
 
 @onready var map_floor: MapFloor = $Floor
 @onready var map_walls: Node2D = $Walls
@@ -12,15 +12,15 @@ const CELL_DEBUG_LABEL_SCENE : PackedScene = preload("uid://s05eepwpbj7e")
 
 func _ready():
 	# sets the current map (self) at the cursor manager
-	SignalBus.main_init_finished.connect(func(): 
-		SignalBus.map_initialized.emit(self))
+	SignalBus.main_init_finished.connect(func():
+		SignalBus.map_initialized.emit(self ))
 	
 	if show_debug_cells:
 		Log.debug("Cell coordinates logging is on!")
 		for x in map_floor.get_used_rect().size.x:
 			for y in map_floor.get_used_rect().size.y:
 				var cell_debug_label = CELL_DEBUG_LABEL_SCENE.instantiate() as CellDebugLabel
-				cell_debug_label.current_cell = Vector2i(x,y)
+				cell_debug_label.current_cell = Vector2i(x, y)
 				cell_debug_label.position = MapHelpers.cell_to_pixel(cell_debug_label.current_cell)
 				self.add_child(cell_debug_label)
 			
@@ -28,7 +28,7 @@ func is_tile_walk_selectable(pos: Vector2i) -> bool:
 	return pathfind.astar_grid.region.has_point(pos) and not pathfind.astar_grid.is_point_solid(pos)
 
 ## Unblocks the from field temporarily
-func get_astar_path(from : Vector2i, to : Vector2i, partial_path : bool = false) -> Array[Vector2i]:
+func get_astar_path(from: Vector2i, to: Vector2i, partial_path: bool = false) -> Array[Vector2i]:
 	pathfind.astar_grid.set_point_solid(from, false)
 	var path = pathfind.astar_grid.get_id_path(from, to, partial_path)
 	pathfind.astar_grid.set_point_solid(from, true)
@@ -36,7 +36,7 @@ func get_astar_path(from : Vector2i, to : Vector2i, partial_path : bool = false)
 
 ## Returns the rect of the floor in pixel world coordinates.
 func get_used_rect() -> Rect2:
-	var used_rect : Rect2i = map_floor.get_used_rect()
+	var used_rect: Rect2i = map_floor.get_used_rect()
 	# Get the top-left position in world coordinates
 	var world_position = map_floor.map_to_local(used_rect.position)
 	# Get the size in world coordinates (pixels)
@@ -46,14 +46,14 @@ func get_used_rect() -> Rect2:
 
 
 # los = line of sight
-func get_los_to_enemies(from : Vector2i, enemy_group_name : String) -> Array:
+func get_los_to_enemies(from: Vector2i, enemy_group_name: String, weapon_range: int = 0) -> Array:
 	var result = []
-	for child in self.get_children(): 
+	for child in self.get_children():
 		var c = child as BaseCharacter
 		if c == null:
 			continue
 		if c.get_groups().has(enemy_group_name):
-			var los = get_line_of_sight(from, c.current_cell, true, true)
+			var los = get_line_of_sight(from, c.current_cell, true, true, weapon_range)
 			if los.size() > 0:
 				result.append(los)
 	return result
@@ -62,7 +62,12 @@ func get_los_to_enemies(from : Vector2i, enemy_group_name : String) -> Array:
 func get_line_of_sight(from_cell: Vector2i,
 	to_cell: Vector2i,
 	ignore_solid_from: bool,
-	ignore_solid_to: bool) -> Array[Vector2]:
+	ignore_solid_to: bool,
+	weapon_range: int = 0) -> Array[Vector2]:
+	if weapon_range > 0:
+		var dist = maxi(abs(to_cell.x - from_cell.x), abs(to_cell.y - from_cell.y))
+		if dist > weapon_range:
+			return []
 	
 	# Compute the segment in world coordinates
 	var segment_start = MapHelpers.cell_to_pixel(from_cell)
